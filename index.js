576db82b64565c92344cbcb6e8b906f8cee8c5f1
+ <salimhetham@gmail.com>
2023-12-02 10:00:52 +0300

    added slice

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let currentUser = "Salim"
 
-let welcomeMessage = "Welcome to Flatbook, Salim!"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, SALIM!"
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-let shortGreeting = "Welcome, S!"
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+let statement = console.log(`Welcome, ${currentUser.slice(0,1)}!`)
